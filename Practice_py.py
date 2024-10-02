58f62be5ae759ba921278a8ba6c671a88f93c630
Anubhav Kushwaha <anubhavkush8@gmail.com>
2024-10-03 00:27:38 +0530

    Update Practice_py.py

--- Practice_py.py
+++ Practice_py.py
@@ -20,23 +20,48 @@
 #   print("You are not eligible.")
 
 # import random
-
 # x = random.choice(["Heads", "Tails"])
 # print(x)
 
 # import random
-
 # friends = ["Anubhav", "Sahil", "Swarnit"]
-
 # card_pick_to_pay_bill = random.choice(friends)
 # print(card_pick_to_pay_bill)
 
+# ls = ["s", "d", "f", "g"]
+# for x in ls[25]:
+#   print(x)
 
+# student_scores =[150,25,234,199,78,45,6,0,235,678,]
+# max = 0
+# for score in student_scores:
+#   if score > max:
+#     max = score
+# print(max)
 
+# for x in range( 0,9):
+#   print(x)
 
+# total = 0
+# for x in range(1, 100+1):
+#   total += x
+  
+# print(total)
 
+# num =0
+# for num in range(0,100+1):
+#     if num%3==0 and num%5==0:
+#       print("FizzBuzz")
+#     elif num%3==0:
+#       print("Fizz")
+#     elif num%5==0:
+#       print("Buzz")
+#     else:
+#       print(num)
 
-
+# for i in range(65, 91):
+#     print(chr(i), end=" ")
+#     print(i[25])
 
 
 
